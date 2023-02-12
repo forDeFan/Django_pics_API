dda90e76a9001e69611524a9f4718c350c030746
tom <t-gutowski@o2.pl>
2023-02-12 17:42:16 +0100

    feat: enable custom admin panel
    Add admin panel with custom fields.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,67 @@
+from core import models
+from typing import List
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group, Permission
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Define users page in admin panel"""
+
+    def group(self, user) -> List:
+        """
+        Custom listing implementation to show user group in
+        admin UI.
+        """
+        groups = []
+        for group in user.groups.all():
+            groups.append(group.name)
+        return " ".join(groups)
+
+    # Column name in admin UI.
+    group.short_description = "Group"
+
+    ordering = ["id"]
+
+    list_display = [
+        "email",
+        "name",
+        "group",
+        "is_active",
+        "is_superuser",
+    ]
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {"fields": ("is_active", "is_staff", "is_superuser")},
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+        (_("User groups"), {"fields": ("groups",)}),
+    )
+
+    readonly_fields = ["last_login"]
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
