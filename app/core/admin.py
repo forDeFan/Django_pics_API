315e8822199503bbf57b4c39c53d28acc57c943d
tom <t-gutowski@o2.pl>
2023-02-12 17:45:20 +0100

    refactor: remove unused imports

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,9 +1,9 @@
-from core import models
 from typing import List
+
+from core import models
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import Group, Permission
 
 
 class UserAdmin(BaseUserAdmin):
