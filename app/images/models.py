0d69488b56713b69e472b2badfda542f788a3de9
tom <t-gutowski@o2.pl>
2023-02-15 18:42:55 +0100

    feat: custom serializers for Image model
    Add custom serializers for image - depending on tier. Applied migrations.

--- app/images/models.py
+++ app/images/models.py
@@ -2,41 +2,49 @@ from django.db import models
 
 
 def user_dir_path(instance, filename: str) -> str:
-    """ 
-    Customized path to upload images to.
     """
-    return "images/{0}".format(filename)
+    Customized path to upload images to.
+    Creates catalogue based on user 'id' value to upload images/ thumbnails in there.
+    'owner' attribute relates to field 'owner' in Image model.
+    """
+    return "images/{0}/{1}".format(instance.owner.id, filename)
 
 
 class Image(models.Model):
     """
     Image model class.
     """
-    name = models.CharField(max_length=100)
-    # core.User to avoid circular import error
-    owner = models.ForeignKey("core.User", on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=user_dir_path)
 
-    class Meta:
-        """ 
-        Make possible for user to keep multiple images ownership
-        associated to his id.
-        """
-        unique_together = ('owner', 'image')
+    name = models.CharField(max_length=100, unique=False)
+    # core.User to avoid circular import error
+    owner = models.ForeignKey("core.User", on_delete=models.CASCADE, editable=False)
+    image_link = models.ImageField(
+        upload_to=user_dir_path, unique=False
+    )
+    thumbnail_small = models.CharField(
+        max_length=255, null=True, blank=True
+    )
+    thumbnail_large = models.CharField(
+        max_length=255, null=True, blank=True
+    )
+    expiring_link = models.CharField(
+        max_length=255, null=True, blank=True
+    )
 
 
 class Tier(models.Model):
-    """ 
+    """
     Tier model class.
     """
+
     name = models.CharField(max_length=100, default="Basic")
-    thumbnail_small = models.BooleanField(default=True)
-    thumbnail_large = models.BooleanField(default=False)
     image_link = models.BooleanField(default=False)
+    thumbnail_small = models.BooleanField(default=True)
+    thumbnail_large = models.BooleanField(default=True)
+    expiring_link = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         """
-        Returns name value in str representation of the Tier object. 
+        Returns name value in str representation of the Tier object.
         """
-        return "%s %s"%(self.name, " tier")
-    
+        return "%s %s" % (self.name, "")
--- app/images/serializers.py
+++ app/images/serializers.py
@@ -1,22 +1,84 @@
-import os
-
 from django.db import models
-from images.models import Image
+from images.models import Image, Tier
 from rest_framework import serializers
-from rest_framework.serializers import ValidationError
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class BasicTierImageSerializer(serializers.ModelSerializer):
+    """Serializer for Image class for user in Basic Tier plan."""
+
     image_path = models.FileField(blank=False, null=False)
 
     class Meta:
         model = Image
-        fields = ("name", "image", "owner")
+        fields = ("id", "image_link", "thumbnail_small")
 
-        def create(self, validated_data):
-            images = validated_data.pop('image')
-            owner = validated_data.pop('owner')
-            for image in images:
-                Image.objects.create(owner=owner, image=image)
-            return validated_data
 
+class PremiumTierImageSerializer(serializers.ModelSerializer):
+    """Serializer for Image class for user in Premium Tier plan."""
+
+    image_path = models.FileField(blank=False, null=False)
+
+    class Meta:
+        model = Image
+        fields = (
+            "id",
+            "image_link",
+            "thumbnail_small",
+            "thumbnail_large",
+        )
+
+
+class EnterpriseTierImageSerializer(serializers.ModelSerializer):
+    """Serializer for Image class for user in Enterprise Tier plan."""
+
+    image_path = models.FileField(blank=False, null=False)
+
+    class Meta:
+        model = Image
+        fields = (
+            "id",
+            "image_link",
+            "thumbnail_small",
+            "thumbnail_large",
+            "expiring_link",
+        )
+
+
+class TierSerializer(serializers.ModelSerializer):
+    """Serializer of Tier class."""
+
+    class Meta:
+        model = Tier
+        fields = "__all__"
+
+
+"""
+POC
+
+from rest_framework import serializers
+
+class MyModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyModel
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        # Call the parent to_representation() method to get the default representation
+        representation = super().to_representation(instance)
+        
+        # Check if the model is valid
+        if instance.is_valid():
+            # If valid, return a subset of the fields
+            representation = {
+                'field1': representation['field1'],
+                'field2': representation['field2']
+            }
+        else:
+            # If invalid, return a different subset of fields
+            representation = {
+                'field1': representation['field1'],
+                'field3': representation['field3']
+            }
+        
+        return representation
+"""
