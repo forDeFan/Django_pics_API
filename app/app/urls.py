92fd803e41d8d4cca88169ca0207919a810d9213
tom <t-gutowski@o2.pl>
2023-02-19 11:56:14 +0100

    feat: attached default django page to url

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,14 +1,16 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
-
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views import debug
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
 
 urlpatterns = [
+    # Default django site - just to see something at app start.
+    path("", debug.default_urlconf),
     path("admin/", admin.site.urls),
     # API docs endpoints
     path(
